7ac768e724eb41987a950abc444cef1747bd0a84
lavertyaf <lavertyaileen@gmail.com>
2018-03-12 17:17:25 +0000

    part 1

--- codeclan_students/codeclan_student.rb
+++ codeclan_students/codeclan_student.rb
@@ -1,1 +1,25 @@
-s
+class CodeclanStudent
+
+  attr_accessor :name, :cohort
+
+  def initialize(input_name, input_cohort, input_fav_language)
+    @name = input_name
+    @cohort = input_cohort
+    @fav_language = input_fav_language
+    # p name()
+    # p cohort()
+  end
+
+  def print_name()
+    puts "My name is #{@name}"
+  end
+
+  def talk()
+    return "I can talk"
+  end
+
+  def favourite_language
+    return "I love #{@fav_language}"
+  end
+
+end
--- codeclan_students/specs/codeclan_student_spec.rb
+++ codeclan_students/specs/codeclan_student_spec.rb
@@ -1,1 +1,63 @@
-s
+require('minitest/autorun')
+require_relative('../codeclan_student.rb')
+
+class TestCodeclanStudents < MiniTest::Test
+
+  def setup
+    @student = CodeclanStudent.new("John", 20, "Ruby")
+    @student2 = CodeclanStudent.new("Aileen", 20, "JavaScript")
+  end
+
+  def test_student_has_name
+    assert_equal("Aileen", @student2.name)
+  end
+
+  def test_student_cohort_number
+    assert_equal(20, @student.cohort)
+  end
+
+  def test_student_has_new_name
+    @student.name = "Jimmy"
+    assert_equal("Jimmy", @student.name)
+  end
+
+  def test_student_new_cohort_number
+    @student.cohort = 30
+    assert_equal(30, @student.cohort)
+  end
+
+  def test_get_new_name
+    @student.print_name
+    @student.name = "Sara"
+    @student.print_name
+    assert_equal("Sara", @student.name)
+  end
+
+  def test_student_talk
+    assert_equal("I can talk", @student.talk)
+  end
+
+  def test_students_favourite_language
+    assert_equal("I love Ruby", @student.favourite_language)
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# ➜  homework git:(master) mkdir codeclan_students
+# ➜  homework git:(master) cd codeclan_students
+# ➜  codeclan_students git:(master) mkdir specs
+# ➜  codeclan_students git:(master) touch specs/codeclan_student_spec.rb
+# ➜  codeclan_students git:(master) ✗ touch codeclan_student.rb
+# ➜  codeclan_students git:(master) ✗ atom .
